db199b963716eb529c6e4cf9dac583fc7b05c1c0
Randito Faustino Roxas <roxas.randito17@gmail.com>
2018-07-25 17:03:37 +0800

    Thread update

--- Caltrain.java
+++ Caltrain.java
@@ -2,49 +2,52 @@ import java.util.Scanner;
 
 public class Caltrain{
 	Scanner reader;	
-	
+	Station[] stations = new Station[8];
+	Train[] trains = new Train[16];
+
 	public Caltrain(){
 		reader = new Scanner(System.in);
+
+		//Create Stations
+		station_init();
+		//Create All Trains
+		train_init();
 	}
 
 	public void run(){
-		//Create Stations
-		Station[] stations = station_init();
-		
-		//Create All Trains
-		Train[] trains = train_init();
+		open_stations();
 
 	}
 
 	////////////////// Create All 8 Stations //////////////////
-	public Station[] station_init(){
-		Station[] t = new Station[8];
-
-		t[0] = new Station("Roosevelt");
-		t[1] = new Station("Monumento");
-		t[2] = new Station("R. Papa");
-		t[3] = new Station("Blumentrit");
-		t[4] = new Station("Bambang");
-		t[5] = new Station("Carriedo");
-		t[6] = new Station("EDSA");
-		t[7] = new Station("Baclaran");
-
-		return t;
+	public void station_init(){
+		this.stations[0] = new Station("Roosevelt");
+		this.stations[1] = new Station("Monumento");
+		this.stations[2] = new Station("R. Papa");
+		this.stations[3] = new Station("Blumentrit");
+		this.stations[4] = new Station("Bambang");
+		this.stations[5] = new Station("Carriedo");
+		this.stations[6] = new Station("EDSA");
+		this.stations[7] = new Station("Baclaran");
 	}
 
 	////////////////// Create All Trains //////////////////
-	public Train[] train_init(){
-		Train[] t = new Train[16];
-		Integer N = 16;
-
+	public void train_init(){
 		for(int i=0; i<16; i++){
 			// Get input
 			System.out.println("Train "+(i+1));
 			System.out.print("Number of Seats: ");
 			int x = reader.nextInt();
-			t[i] = new Train(x);
+			this.trains[i] = new Train(x);
 		}
-		return t;
+	}
+	////////////////// Open All Stations //////////////////
+	public void open_stations(){
+		
+		for(int i=0; i<8; i++){
+			stations[i].open();
+		}
+
 	}
 
 	////////////////// A train arrives at the station; count=seats and is treated as an input //////////
