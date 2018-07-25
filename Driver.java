1a97b9f87510fa7cdfa16cf58d7f5b6af45e479d
Brand4i13a <brand_teh@users.noreply.github.com>
2018-07-25 16:11:23 +0800

    Driver

---
+++ Driver.java
@@ -0,0 +1,11 @@
+package controller;
+import model.Caltrain;
+
+public class Driver{
+	
+	public static void main(String[]args){
+		//instantiate caltrain
+		Caltrain caltrain = new Caltrain();
+		caltrain.run();
+	}
+}
