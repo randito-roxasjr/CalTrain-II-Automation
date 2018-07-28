eb5ba22ef661fd5abf86854d6dd014835343a652
Randito Faustino Roxas <randito_roxasjr@dlsu.edu.ph>
2018-07-28 09:31:58 +0800

    last update

--- Caltrain.java
+++ Caltrain.java
@@ -51,21 +51,61 @@ public class Caltrain{
 	}
 
 	////////////////// A train arrives at the station; count=seats and is treated as an input //////////
-	public void station_load_train(int count){
-		;
+	public void station_load_train(int count){	
+		/* Has no available seat, just let the train pass by. */
+		if (count <= 0)
+			return;
+
+		lock_acquire(&station->lock);
+		station->free_seats = count;
+		station->boarding_passengers = 0;
+
+		while (station->waiting_passengers > 0 && station->free_seats > 0) {
+			/* Notify there are available seats. */
+			cond_signal(&station->has_waiting_seats, &station->lock);
+			lock_release(&station->lock);
+			lock_acquire(&station->lock);
+		}
+
+			
+		/* Waiting until all passengers onboarded. */
+		while (station->boarding_passengers > 0) {
+			lock_release(&station->lock);
+			lock_acquire(&station->lock);
+	    }
+
+		/* Train is leaving, mark there is no seat available. */
+		station->free_seats = 0;
+		lock_release(&station->lock);		
 	}
 
 	////////////////// station waits for the train //////////////////
 	public void station_wait_for_train(){
-		// If train has arrived and there are enough seats		
-		// else, continue to wait
-		;
+		lock_acquire(&station->lock);
+
+		/* Wait for seat. */
+		station->waiting_passengers = station->waiting_passengers + 1;
+		
+		while (station->free_seats <= 0) {
+			cond_wait(&station->has_waiting_seats, &station->lock);
+		}
+
+		/* Book the seat. */
+		station->free_seats = station->free_seats - 1;
+		/* Boarding */
+	
+		station->boarding_passengers = station->boarding_passengers + 1;
+		lock_release(&station->lock);
+		// Will call station_on_board after this returns.
 	}
 
 	////////////////// Passengers are seated //////////////////
 	public void station_on_board(){
 		// Let train know passengers are on board
-		;
+		lock_acquire(&station->lock);
+		station->waiting_passengers = station->waiting_passengers - 1;
+		station->boarding_passengers = station->boarding_passengers - 1;
+		lock_release(&station->lock);
 	}
 
 }
--- Passenger.java
+++ Passenger.java
@@ -3,6 +3,7 @@ import java.util.concurrent.*;
 class Passenger implements Runnable{
 	private String name;
 	private Thread robot;
+	private boolean active;
 	private Semaphore sem = new Semaphore(1);
 
 	Passenger(String name){
