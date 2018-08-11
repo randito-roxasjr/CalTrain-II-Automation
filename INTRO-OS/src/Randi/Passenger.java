c08fd45b67791c36f264dcc277ea5648c825e365
Dean <dean_camelo@dlsu.edu.ph>
2018-08-11 16:58:36 +0800

    backup to master

--- INTRO-OS/src/Randi/Passenger.java
+++ INTRO-OS/src/Randi/Passenger.java
@@ -8,13 +8,15 @@ class Passenger extends Thread{
 	private Train train;
 	View view;
 	int curr;
-	
-	Passenger(String name, Station station, Station drop_station, View panel, int num){
+	int drop;
+
+	Passenger(String name, Station station, Station drop_station, View panel, int num, int num2){
 		this.view = panel;
 		this.name = name;
 		this.station = station;
 		this.drop_station = drop_station;
 		this.curr = num;
+		this.drop = num2;
 	}
 
 	public void run(){
@@ -22,10 +24,10 @@ class Passenger extends Thread{
 		drop_station.incrementDropOff(train.name);
 		station.station_on_board();
 		view.pass_station[curr]--;
-		
-	//	while(drop_station.station_check_station(train.curr_station)); 
+
+	//	while(drop_station.station_check_station(train.curr_station));
 		drop_station.station_get_off(train.name);
 		System.out.println("Passenger " + name + " dropped off.");
-		view.drop_train[curr]++;
+		view.drop_train[drop]++;
 	}
 }
